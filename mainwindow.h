20f5a798c731230a46c7f7e0e4357a5986aa8c49
syntexsquad <sjs100788@gmail.com>
2025-02-11 15:53:35 +0600

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,1 +1,24 @@
 
+#include <QMainWindow>
+#include <QSet>
+#include <QMap>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void button1(); // Book access check
+    void onRegisterButtonClicked(); // Show registration fields
+    void onConfirmButtonClicked(); // Register roll numbers
+    void on_AddBookButton_clicked(); // Show add book fields
+    void on_ADDButton_clicked(); // Add book to library
+
