27e145aed35424d435cd3792141092ca6ff6c7af
srabir123 <abirahmed2777@gmail.com>
2025-02-11 15:28:13 +0600

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,1 +1,12 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug> // For debugging output
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
 
