447ed790e6e8eb30ffadd1c3e1a4728fef995225
abuzafor7 <abuzaforabir@gmail.com>
2025-02-12 11:32:17 +0600

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -21,4 +21,20 @@ private slots:
     void onConfirmButtonClicked(); // Register roll numbers
     void on_AddBookButton_clicked(); // Show add book fields
     void on_ADDButton_clicked(); // Add book to library
+    void on_CHECKAVAILABILYbutton_clicked(); // Handles the CHECK AVAILABILITY button click
+    void checkBookAvailability(const QString &bookName); // Checks book availability in the library
+    void on_searchbutton_clicked(); // Handles the search button click (NEW)
+
+
+private:
+    Ui::MainWindow *ui;
+    QSet<int> registeredRolls; // Stores registered roll numbers
+    QMap<QString, QString> bookShelfMap; // Stores book name and shelf details
+
+    void loadRegisteredRolls(); // Load registered rolls from file
+    void saveRegisteredRolls(); // Save registered rolls to file
+    void loadBooks(); // Load books from file
+};
+
+#endif // MAINWINDOW_H
 
